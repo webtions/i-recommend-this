9dea91ea83b8e336ac44ad111b6f5ddee4b34d72
hchouhan <hchouhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-07 20:18:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/i-recommend-this/trunk@541074 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- i-recommend-this.php
+++ i-recommend-this.php
@@ -3,7 +3,7 @@
 Plugin Name: I Recommend This
 Plugin URI: http://www.harishchouhan.com/personal-projects/i-recommend-this/
 Description: This plugin allows your visitors to simply recommend or like your posts instead of commment it.
-Version: 1.3
+Version: 1.3.1
 Author: Harish Chouhan
 Author URI: http://www.harishchouhan.com
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.designerskiosk.com
 Tags: recommend, like, love, post, rate, rating, heart
 Requires at least: 3.0
 Tested up to: 3.2.2
-Stable tag: 1.3
+Stable tag: 1.3.1
 Last Updated: 2012-May-7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
