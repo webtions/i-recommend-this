1e1e797f09354856fd257f8512a989a1fd9cceec
hchouhan <hchouhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-14 05:37:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/i-recommend-this/trunk@543958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.designerskiosk.com
 Tags: recommend, like, love, post, rate, rating, heart
 Requires at least: 3.0
 Tested up to: 3.2.2
-Stable tag: 1.3.1
+Stable tag: 1.4
 Last Updated: 2012-May-7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -59,6 +59,9 @@ You can also visit the [support center](http://www.harishchouhan.com/personal-pr
 
 
 == Changelog ==
+
+= 1.4
+* Added feature to display custom text when a post is liked.
 
 = 1.3
 * Removed 2 functions "register_widget_control()" & "register_sidebar_widget()" deprecated in version 2.8 with latest functions
--- recommend.php
+++ recommend.php
@@ -4,6 +4,7 @@ require_once '../../../wp-config.php';
 global $wpdb;
 $post_ID = $_POST['id'];
 $ip = $_SERVER['REMOTE_ADDR'];
+$irt_textOnclick = get_option('irt_textOnclick');
 $recommend = get_post_meta($post_ID, '_recommended', true);
 
 if($post_ID != '') {
@@ -13,13 +14,15 @@ if($post_ID != '') {
 		$recommendNew = $recommend + 1;
 		update_post_meta($post_ID, '_recommended', $recommendNew);
 
-		setcookie('liked-'.$post_ID, time(), time()+3600*24*365, '/');
+		setcookie('recommended-'.$post_ID, time(), time()+3600*24*365, '/');
 		$wpdb->query("INSERT INTO ".$wpdb->prefix."irecommendthis_votes VALUES ('', NOW(), '$post_ID', '$ip')");
 
-		echo $recommendNew.' recommends'; // .' lights' is added new
+		$return_text = $recommendNew . ' ' . $irt_textOnclick;
+		echo $return_text; //$recommendNew; // 
 	}
 	else {
-		echo $recommend.' recommends'; // .' lights' is added new
+		$return_text = $recommend . ' ' . $irt_textOnclick;
+		echo $return_text; //$recommendNew; //  
 	}
 }
 ?>
