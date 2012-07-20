e46f0dbdc07ca5d975bcc009980918466ad7fd00
hchouhan <hchouhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-20 13:44:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/i-recommend-this/trunk@575144 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- i-recommend-this.php
+++ i-recommend-this.php
@@ -3,7 +3,7 @@
 Plugin Name: I Recommend This
 Plugin URI: http://www.harishchouhan.com/personal-projects/i-recommend-this/
 Description: This plugin allows your visitors to simply recommend or like your posts instead of commment it.
-Version: 1.4.3
+Version: 1.4.4
 Author: Harish Chouhan
 Author URI: http://www.harishchouhan.com
 
@@ -363,4 +363,56 @@ function addHeaderLinks() {
 
 add_action('init', enqueueScripts);
 add_action('wp_head', addHeaderLinks);
+
+// Start of Presstrends Magic
+function presstrends_plugin() {
+
+// PressTrends Account API Key
+$api_key = '0o9bxvlwkq4x71se6ey7ggnrjckfsmzs89k8';
+$auth = 'wu9y7j98r8vkpgl7e3dsw0hzbhbpibdg3';
+
+// Start of Metrics
+global $wpdb;
+$data = get_transient( 'presstrends_data' );
+if (!$data || $data == ''){
+$api_base = 'http://api.presstrends.io/index.php/api/pluginsites/update/auth/';
+$url = $api_base . $auth . '/api/' . $api_key . '/';
+$data = array();
+$count_posts = wp_count_posts();
+$count_pages = wp_count_posts('page');
+$comments_count = wp_count_comments();
+$theme_data = get_theme_data(get_stylesheet_directory() . '/style.css');
+$plugin_count = count(get_option('active_plugins'));
+$all_plugins = get_plugins();
+foreach($all_plugins as $plugin_file => $plugin_data) {
+$plugin_name .= $plugin_data['Name'];
+$plugin_name .= '&';}
+$plugin_data = get_plugin_data( __FILE__ );
+$plugin_version = $plugin_data['Version'];
+$posts_with_comments = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type='post' AND comment_count > 0");
+$comments_to_posts = number_format(($posts_with_comments / $count_posts->publish) * 100, 0, '.', '');
+$pingback_result = $wpdb->get_var('SELECT COUNT(comment_ID) FROM '.$wpdb->comments.' WHERE comment_type = "pingback"');
+$data['url'] = stripslashes(str_replace(array('http://', '/', ':' ), '', site_url()));
+$data['posts'] = $count_posts->publish;
+$data['pages'] = $count_pages->publish;
+$data['comments'] = $comments_count->total_comments;
+$data['approved'] = $comments_count->approved;
+$data['spam'] = $comments_count->spam;
+$data['pingbacks'] = $pingback_result;
+$data['post_conversion'] = $comments_to_posts;
+$data['theme_version'] = $plugin_version;
+$data['theme_name'] = urlencode($theme_data['Name']);
+$data['site_name'] = str_replace( ' ', '', get_bloginfo( 'name' ));
+$data['plugins'] = $plugin_count;
+$data['plugin'] = urlencode($plugin_name);
+$data['wpversion'] = get_bloginfo('version');
+foreach ( $data as $k => $v ) {
+$url .= $k . '/' . $v . '/';}
+$response = wp_remote_get( $url );
+set_transient('presstrends_data', $data, 60*60*24);}
+}
+
+// PressTrends WordPress Action
+add_action('admin_init', 'presstrends_plugin');
+		
 ?>
