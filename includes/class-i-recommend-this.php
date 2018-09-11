a71d6bf80fdc786869ebe907bfb347e5dbdcee02
Harish Chouhan <hello@dreamsmedia.in>
2018-09-11 22:44:16 +0700

    Update tag to 3.8.2

--- includes/class-i-recommend-this.php
+++ includes/class-i-recommend-this.php
@@ -17,7 +17,7 @@ class Themeist_IRecommendThis {
 		if ( defined( 'THEMEIST_IRT_VERSION' ) ) {
 			$this->version = THEMEIST_IRT_VERSION;
 		} else {
-			$this->version = '3.8.1';
+			$this->version = '3.8.2';
 		}
 
 		if ( defined( 'THEMEIST_IRT_DB_VERSION' ) ) {
