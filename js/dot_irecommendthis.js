7cf315422a80cdf0a44af9fe539998c3346c3edb
Edmund Mai <edmundmai@gmail.com>
2018-12-07 14:11:46 -0500

    Adds support for multiple buttons on the same page

--- js/dot_irecommendthis.js
+++ js/dot_irecommendthis.js
@@ -1,17 +1,22 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
+  $(document).on("click", ".dot-irecommendthis", function() {
+    var link = $(this);
+    if (link.hasClass("active")) return false;
 
-	$(document).on('click', '.dot-irecommendthis',function() {
-		var link = $(this);
-		if(link.hasClass('active')) return false;
-		
-		var id = $(this).attr('id'),
-			suffix = link.find('.dot-irecommendthis-suffix').text();
-		
-		$.post(dot_irecommendthis.ajaxurl, { action:'dot-irecommendthis', recommend_id:id, suffix:suffix }, function(data){
-			link.html(data).addClass('active').attr('title','You already recommended this');
-		});
-		
-		return false;
-	});
- 
+    var id = $(this).attr("id"),
+      suffix = link.find(".dot-irecommendthis-suffix").text();
+
+    $.post(
+      dot_irecommendthis.ajaxurl,
+      { action: "dot-irecommendthis", recommend_id: id, suffix: suffix },
+      function(data) {
+        $(".dot-irecommendthis")
+          .html(data)
+          .addClass("active")
+          .attr("title", "You already recommended this");
+      }
+    );
+
+    return false;
+  });
 });
