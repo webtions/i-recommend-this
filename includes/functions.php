5cc138e908bbac583c6f7444891d9777ce42134a
Harish Chouhan <me@harishchouhan.com>
2024-06-25 19:34:21 +0700

    Code cleanup

--- includes/functions.php
+++ includes/functions.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * Functions for I Recommend This plugin
+ *
+ * @package IRecommendThis
+ */
 
+if ( ! function_exists( 'themeist_i_recommend_this_public' ) ) {
+	/**
+	 * Output the public view of the plugin.
+	 *
+	 * This function outputs the value of the global variable
+	 * $themeist_i_recommend_this_public, which is expected to
+	 * be defined and populated elsewhere in the plugin.
+	 */
+	function themeist_i_recommend_this_public() {
+		global $themeist_i_recommend_this_public;
 
-
-/*
---------------------------------------------*
- * Template Tag
- *--------------------------------------------*/
-
-function dot_irecommendthis( $id = null ) {
-	global $themeist_i_recommend_this_public;
-	echo $themeist_i_recommend_this_public->dot_recommend( $id );
+		// Escape the output to allow safe HTML rendering.
+		echo wp_kses_post( $themeist_i_recommend_this_public );
+	}
 }
