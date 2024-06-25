98995689812891cd74f7a273351e1dba73badd0b
Harish Chouhan <me@harishchouhan.com>
2024-06-25 23:07:22 +0700

    Fixed errors based on PHPCS

--- public/register-widget.php
+++ public/register-widget.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * Register the widget.
+ *
+ * @package IRecommendThis
+ */
+
+/**
+ * Register the Most Recommended Posts widget.
  */
 function widget_most_recommended_posts() {
 	register_widget( 'Themeist_Most_Recommended_Posts_Widget' );
