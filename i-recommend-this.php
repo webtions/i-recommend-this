c2be90728e1f3d8ab4a75c61f1abaa567422bb8a
Harish Chouhan <me@harishchouhan.com>
2024-06-26 19:22:42 +0700

    Cleanup

--- i-recommend-this.php
+++ i-recommend-this.php
@@ -11,40 +11,39 @@ Text Domain: i-recommend-this
 License: GPL-3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 Domain Path: /languages
-
-phpcs:disable PSR1.Files.SideEffects
 */
 
-if ( ! defined('WPINC') ) {
-	die;
+// Exit if accessed directly.
+if (! defined('ABSPATH') ) {
+	exit;
 }
 
-// do nothing if class is already defined
-if(class_exists('Themeist_IRecommendThis') ) {
-	return;
-}
+// Check if class already exists to prevent conflicts.
+if (! class_exists('Themeist_IRecommendThis') ) :
 
-define('THEMEIST_IRT_VERSION', '3.9.0');
-   define('THEMEIST_IRT_DB_VERSION', '2.6.2'); 
+	define('THEMEIST_IRT_VERSION', '3.9.0');
+	define('THEMEIST_IRT_DB_VERSION', '2.6.2');
 
-// require includes
-require_once dirname( __FILE__ ) . '/includes/class-i-recommend-this.php';
-require_once dirname( __FILE__ ) . '/admin/class-i-recommend-this-admin.php';
-require_once dirname( __FILE__ ) . '/public/class-i-recommend-this-public.php';
-require_once dirname( __FILE__ ) . '/public/class-widget-most-recommended-posts.php';
-require_once dirname( __FILE__ ) . '/includes/functions.php';
+	// Require includes
+	include_once plugin_dir_path(__FILE__) . 'includes/class-i-recommend-this.php';
+	include_once plugin_dir_path(__FILE__) . 'admin/class-i-recommend-this-admin.php';
+	include_once plugin_dir_path(__FILE__) . 'public/class-i-recommend-this-public.php';
+	include_once plugin_dir_path(__FILE__) . 'public/class-widget-most-recommended-posts.php';
+	include_once plugin_dir_path(__FILE__) . 'includes/functions.php';
 
-// create instance of plugin class
-global $themeist_i_recommend_this;
-$themeist_i_recommend_this = new Themeist_IRecommendThis(__FILE__);
-$themeist_i_recommend_this->add_hooks();
+	// Create instance of the main plugin class
+	global $themeist_i_recommend_this;
+	$themeist_i_recommend_this = new Themeist_IRecommendThis(__FILE__);
+	$themeist_i_recommend_this->add_hooks();
 
-// create instance of plugin class
-global $themeist_i_recommend_this_admin;
-$themeist_i_recommend_this_admin = new Themeist_IRecommendThis_Admin(__FILE__);
-$themeist_i_recommend_this_admin->add_admin_hooks();
+	// Create instance of the admin class
+	global $themeist_i_recommend_this_admin;
+	$themeist_i_recommend_this_admin = new Themeist_IRecommendThis_Admin(__FILE__);
+	$themeist_i_recommend_this_admin->add_admin_hooks();
 
-// create instance of plugin class
-global $themeist_i_recommend_this_public;
-$themeist_i_recommend_this_public = new Themeist_IRecommendThis_Public(__FILE__);
-$themeist_i_recommend_this_public->add_public_hooks();
+	// Create instance of the public class
+	global $themeist_i_recommend_this_public;
+	$themeist_i_recommend_this_public = new Themeist_IRecommendThis_Public(__FILE__);
+	$themeist_i_recommend_this_public->add_public_hooks();
+
+endif; // End if class_exists check
