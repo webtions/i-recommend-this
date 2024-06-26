bb46bc105b26af08f41c6da680b287048fcda922
Harish Chouhan <me@harishchouhan.com>
2024-06-26 19:24:44 +0700

    Added a check to ensure the  global object is set before calling its method. This prevents potential errors if the global object is not initialized properly.

--- includes/functions.php
+++ includes/functions.php
@@ -1,14 +1,19 @@
 <?php
 
-
-
 /*--------------------------------------------*
  * Template Tag
  *--------------------------------------------*/
 
-function dot_irecommendthis($id = null)
-{
-	global $themeist_i_recommend_this_public;
-	echo $themeist_i_recommend_this_public->dot_recommend($id);
+/**
+ * Outputs the recommendation button for a post.
+ *
+ * @param int|null $id The post ID. If null, the current post ID is used.
+ */
+function dot_irecommendthis($id = null) {
+    global $themeist_i_recommend_this_public;
 
+    // Ensure the global object is available before using it
+    if ( isset( $themeist_i_recommend_this_public ) ) {
+        echo $themeist_i_recommend_this_public->dot_recommend( $id );
+    }
 }
