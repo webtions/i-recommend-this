4dd890083f4575caa137428db01f8f67850b7544
Harish Chouhan <me@harishchouhan.com>
2024-06-27 21:18:42 +0700

    Added debounce function to prevent multiple rapid clicks that could inflate the like count.

--- js/dot_irecommendthis.js
+++ js/dot_irecommendthis.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
-	$(document).on('click', '.dot-irecommendthis', function() {
+	$(document).on('click', '.dot-irecommendthis', function(event) {
+		event.preventDefault();
 		var link = $(this);
+
+		// Check if the link is already processing
+		if (link.hasClass('processing')) {
+			return false;
+		}
+
 		var unrecommend = link.hasClass('active');
 		var id = $(this).attr('id');
 		var suffix = link.find('.dot-irecommendthis-suffix').text();
@@ -8,18 +15,23 @@ jQuery(document).ready(function($) {
 		// Generate a nonce
 		var nonce = dot_irecommendthis.nonce;
 
+		// Add processing class to disable further clicks
+		link.addClass('processing');
+
 		$.post(dot_irecommendthis.ajaxurl, {
 			action: 'dot-irecommendthis',
 			recommend_id: id,
 			suffix: suffix,
 			unrecommend: unrecommend,
-			security: nonce,  // Pass the nonce as part of the request
+			security: nonce,
 		}, function(data) {
-			// Update all like buttons with the same post ID
-			$('.dot-irecommendthis[id="' + id + '"]').each(function() {
-				let title = unrecommend ? "Recommend this" : "You already recommended this";
-				$(this).html(data).toggleClass('active').attr('title', title);
-			});
+			let title = unrecommend ? "Recommend this" : "You already recommended this";
+
+			// Update all buttons with the same id
+			$('.dot-irecommendthis[id="' + id + '"]').html(data).toggleClass('active').attr('title', title);
+
+			// Remove processing class to allow future clicks
+			$('.dot-irecommendthis[id="' + id + '"]').removeClass('processing');
 		});
 
 		return false;
