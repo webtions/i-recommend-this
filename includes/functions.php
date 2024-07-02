bd59f607196332c80851f30befb11393061d35f9
Harish Chouhan <me@harishchouhan.com>
2024-07-03 01:39:59 +0700

    Update code based on PHPCS errors

--- includes/functions.php
+++ includes/functions.php
@@ -1,14 +1,9 @@
 <?php
-
-/*--------------------------------------------*
- * Template Tag
- *--------------------------------------------*/
-
 /**
- * Outputs the recommendation button for a post.
+ * Template tag to output the recommendation button for a post.
  *
  * @param int|null $id The post ID. If null, the current post ID is used.
  */
-function dot_irecommendthis($id = null) {
-    return Themeist_IRecommendThis_Shortcodes::dot_recommend($id);
+function dot_irecommendthis( $id = null ) {
+	return Themeist_IRecommendThis_Shortcodes::dot_recommend( $id );
 }
