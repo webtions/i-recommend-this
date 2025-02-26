66d706155202b014d30d92936b07f19cf601a823
Harish Chouhan <me@harishchouhan.com>
2025-02-26 19:08:09 +0700

    Bump version to 3.10.3 with query block fixes and database optimizations
    - Fixed duplicate post IDs in query blocks
    - Added context awareness for post IDs in query loops
    - Enhanced shortcode functionality
    - Added JavaScript fallback detection
    - Improved database performance

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'e3abb8bd28101c7b0320');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '9b19ebb15d1d94d9260f');
