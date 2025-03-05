8dfad563dd34927c686b22da7ec43a0eaa810925
Harish Chouhan <me@harishchouhan.com>
2025-03-05 15:48:25 +0700

    Version update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '9b19ebb15d1d94d9260f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0c5d168e0530ec6ae3b4');
