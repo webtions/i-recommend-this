3657d3ae135dbccf7e21839830cf9181ac95ea79
Harish Chouhan <me@harishchouhan.com>
2025-03-12 21:22:21 +0700

    Improve widget accessibility and error handling
    - Add ARIA attributes for screen readers
    - Enhance markup for recommendation count display
    - Add option to show/hide empty state messages
    - Implement error handling for no results
    - Improve HTML structure with better classes
    - Add proper data validation and type casting
    - Fix inconsistent HTML output

--- public/class-themeist-irecommendthis-widget-most-recommended.php
+++ public/class-themeist-irecommendthis-widget-most-recommended.php
@@ -19,7 +19,8 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 			__( 'Most Recommended Posts', 'i-recommend-this' ),
 			array(
 				'customize_selective_refresh' => true,
-				'classname' => 'widget_irecommendthis_most_recommended',
+				'classname'                   => 'widget_irecommendthis_most_recommended',
+				'description'                 => __( 'Display the most recommended posts.', 'i-recommend-this' ),
 			)
 		);
 
@@ -40,9 +41,10 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 	public function form( $instance ) {
 		// Set widget defaults.
 		$defaults = array(
-			'title'           => '',
+			'title'           => __( 'Most Recommended Posts', 'i-recommend-this' ),
 			'number_of_posts' => '5',
-			'show_count'      => '',
+			'show_count'      => true,
+			'show_empty'      => false,
 		);
 
 		/**
@@ -55,9 +57,10 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 
 		// Parse current settings with defaults.
 		$instance        = wp_parse_args( (array) $instance, $defaults );
-		$title           = $instance['title'];
-		$number_of_posts = $instance['number_of_posts'];
-		$show_count      = $instance['show_count'];
+		$title           = sanitize_text_field( $instance['title'] );
+		$number_of_posts = absint( $instance['number_of_posts'] );
+		$show_count      = (bool) $instance['show_count'];
+		$show_empty      = (bool) $instance['show_empty'];
 
 		/**
 		 * Action fired before widget form is rendered.
@@ -70,20 +73,26 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 
 		<!-- Widget Title -->
 		<p>
-			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Widget Title', 'i-recommend-this' ); ?></label>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Widget Title:', 'i-recommend-this' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 
 		<!-- Number of Posts to show -->
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'number_of_posts' ) ); ?>"><?php esc_html_e( 'Number of posts to show:', 'i-recommend-this' ); ?></label>
-			<input class="tiny-text" id="<?php echo esc_attr( $this->get_field_id( 'number_of_posts' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number_of_posts' ) ); ?>" type="text" value="<?php echo esc_attr( $number_of_posts ); ?>" />
+			<input class="tiny-text" id="<?php echo esc_attr( $this->get_field_id( 'number_of_posts' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number_of_posts' ) ); ?>" type="number" min="1" max="50" value="<?php echo esc_attr( $number_of_posts ); ?>" />
 		</p>
 
 		<!-- Show number of recommends -->
 		<p>
-			<input id="<?php echo esc_attr( $this->get_field_id( 'show_count' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_count' ) ); ?>" type="checkbox" value="1" <?php checked( '1', $show_count ); ?> />
+			<input id="<?php echo esc_attr( $this->get_field_id( 'show_count' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_count' ) ); ?>" type="checkbox" value="1" <?php checked( true, $show_count ); ?> />
 			<label for="<?php echo esc_attr( $this->get_field_id( 'show_count' ) ); ?>"><?php esc_html_e( 'Show number of recommends?', 'i-recommend-this' ); ?></label>
+		</p>
+
+		<!-- Show message when no recommended posts found -->
+		<p>
+			<input id="<?php echo esc_attr( $this->get_field_id( 'show_empty' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_empty' ) ); ?>" type="checkbox" value="1" <?php checked( true, $show_empty ); ?> />
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_empty' ) ); ?>"><?php esc_html_e( 'Show message when no posts found?', 'i-recommend-this' ); ?></label>
 		</p>
 
 		<?php
@@ -126,6 +135,7 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		$instance['title']           = isset( $new_instance['title'] ) ? sanitize_text_field( $new_instance['title'] ) : '';
 		$instance['number_of_posts'] = isset( $new_instance['number_of_posts'] ) ? absint( $new_instance['number_of_posts'] ) : 5;
 		$instance['show_count']      = isset( $new_instance['show_count'] ) ? (bool) $new_instance['show_count'] : false;
+		$instance['show_empty']      = isset( $new_instance['show_empty'] ) ? (bool) $new_instance['show_empty'] : false;
 
 		/**
 		 * Filter widget settings before saving.
@@ -141,7 +151,7 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		 * Action fired after widget settings are updated.
 		 *
 		 * @since 4.0.0
-		 * @param array $instance Updated widget settings.
+		 * @param array $instance     Updated widget settings.
 		 * @param array $old_instance Previous widget settings.
 		 */
 		do_action( 'irecommendthis_after_widget_update', $instance, $old_instance );
@@ -152,13 +162,14 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 	/**
 	 * Display the widget.
 	 *
-	 * @param array $args Display arguments.
+	 * @param array $args     Display arguments.
 	 * @param array $instance Widget settings.
 	 */
 	public function widget( $args, $instance ) {
 		$title           = ! empty( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
 		$number_of_posts = ! empty( $instance['number_of_posts'] ) ? absint( $instance['number_of_posts'] ) : 5;
-		$show_count      = ! empty( $instance['show_count'] ) ? true : false;
+		$show_count      = isset( $instance['show_count'] ) ? (bool) $instance['show_count'] : true;
+		$show_empty      = isset( $instance['show_empty'] ) ? (bool) $instance['show_empty'] : false;
 
 		/**
 		 * Filter widget display arguments.
@@ -182,7 +193,7 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		echo wp_kses_post( $args['before_widget'] );
 
 		// Display the widget.
-		echo '<div class="widget-text wp_widget_plugin_box">';
+		echo '<div class="widget-text irecommendthis-widget">';
 
 		if ( $title ) {
 			/**
@@ -195,7 +206,9 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 			 */
 			$title = apply_filters( 'irecommendthis_widget_title', $title, $instance, $args );
 
-			echo wp_kses_post( $args['before_title'] ) . esc_html( $title ) . wp_kses_post( $args['after_title'] );
+			echo wp_kses_post( $args['before_title'] ) .
+				'<span class="irecommendthis-widget-title">' . esc_html( $title ) . '</span>' .
+				wp_kses_post( $args['after_title'] );
 		}
 
 		/**
@@ -210,7 +223,8 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		 * Filter the widget list HTML tag.
 		 *
 		 * @since 4.0.0
-		 * @param string $list_tag The HTML tag for the list.
+		 *
+* @param string $list_tag The HTML tag for the list.
 		 */
 		$list_tag = apply_filters( 'irecommendthis_widget_list_tag', 'ul' );
 
@@ -222,16 +236,14 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		 */
 		$list_class = apply_filters( 'irecommendthis_widget_list_class', 'irecommendthis-most-recommended-posts' );
 
-		echo '<' . esc_attr( $list_tag ) . ' class="' . esc_attr( $list_class ) . '">';
-
 		global $wpdb;
 
 		/**
 		 * Filter the SQL query for the widget.
 		 *
 		 * @since 4.0.0
-		 * @param string $sql           The SQL query.
-		 * @param int    $number_of_posts Number of posts to show.
+		 * @param string $sql              The SQL query.
+		 * @param int    $number_of_posts  Number of posts to show.
 		 */
 		$sql = apply_filters(
 			'irecommendthis_widget_query',
@@ -261,76 +273,103 @@ class Themeist_IRecommendThis_Widget_Most_Recommended extends WP_Widget {
 		 */
 		$posts = apply_filters( 'irecommendthis_widget_posts', $posts, $instance );
 
-		foreach ( $posts as $item ) {
-			$post_title = get_the_title( $item->ID );
-			$permalink  = get_permalink( $item->ID );
-			$post_count = absint( $item->meta_value );
-
-			/**
-			 * Filter the widget item HTML tag.
-			 *
-			 * @since 4.0.0
-			 * @param string $item_tag The HTML tag for each item.
-			 */
-			$item_tag = apply_filters( 'irecommendthis_widget_item_tag', 'li' );
-
-			/**
-			 * Action fired before widget item.
-			 *
-			 * @since 4.0.0
-			 * @param object $item The current post item.
-			 */
-			do_action( 'irecommendthis_before_widget_item', $item );
-
-			echo '<' . esc_attr( $item_tag ) . '>';
-
-			/**
-			 * Filter the widget item link.
-			 *
-			 * @since 4.0.0
-			 * @param string $link_html The link HTML.
-			 * @param object $item      The current post item.
-			 */
-			$link_html = apply_filters(
-				'irecommendthis_widget_item_link',
-				'<a href="' . esc_url( $permalink ) . '" title="' . esc_attr( $post_title ) . '" rel="nofollow">' . esc_html( $post_title ) . '</a>',
-				$item
+		// Check if there are any posts.
+		if ( empty( $posts ) ) {
+			if ( $show_empty ) {
+				echo '<p class="irecommendthis-no-posts">' .
+					esc_html__( 'No recommended posts found.', 'i-recommend-this' ) .
+					'</p>';
+			}
+		} else {
+			// Output the list with proper ARIA attributes for accessibility.
+			printf(
+				'<%1$s class="%2$s" aria-label="%3$s">',
+				esc_attr( $list_tag ),
+				esc_attr( $list_class ),
+				esc_attr__( 'Most recommended posts', 'i-recommend-this' )
 			);
 
-			echo wp_kses_post( $link_html );
+			foreach ( $posts as $item ) {
+				$post_title = get_the_title( $item->ID );
+				$permalink  = get_permalink( $item->ID );
+				$post_count = absint( $item->meta_value );
 
-			// Show number of recommends if $show_count is true.
-			if ( $show_count ) {
 				/**
-				 * Filter the widget item count HTML.
+				 * Filter the widget item HTML tag.
 				 *
 				 * @since 4.0.0
-				 * @param string $count_html The count HTML.
-				 * @param int    $post_count The post count.
-				 * @param object $item       The current post item.
+				 * @param string $item_tag The HTML tag for each item.
 				 */
-				$count_html = apply_filters(
-					'irecommendthis_widget_item_count',
-					' (' . esc_html( $post_count ) . ')',
-					$post_count,
+				$item_tag = apply_filters( 'irecommendthis_widget_item_tag', 'li' );
+
+				/**
+				 * Action fired before widget item.
+				 *
+				 * @since 4.0.0
+				 * @param object $item The current post item.
+				 */
+				do_action( 'irecommendthis_before_widget_item', $item );
+
+				printf( '<%s>', esc_attr( $item_tag ) );
+
+				/**
+				 * Filter the widget item link.
+				 *
+				 * @since 4.0.0
+				 * @param string $link_html The link HTML.
+				 * @param object $item      The current post item.
+				 */
+				$link_html = apply_filters(
+					'irecommendthis_widget_item_link',
+					sprintf(
+						'<a href="%1$s" title="%2$s" class="irecommendthis-post-link">%3$s</a>',
+						esc_url( $permalink ),
+						esc_attr( $post_title ),
+						esc_html( $post_title )
+					),
 					$item
 				);
 
-				echo wp_kses_post( $count_html );
-			}
+				echo wp_kses_post( $link_html );
 
-			echo '</' . esc_attr( $item_tag ) . '>';
+				// Show number of recommends if $show_count is true.
+				if ( $show_count ) {
+					/**
+					 * Filter the widget item count HTML.
+					 *
+					 * @since 4.0.0
+					 * @param string $count_html The count HTML.
+					 * @param int    $post_count The post count.
+					 * @param object $item       The current post item.
+					 */
+					$count_html = apply_filters(
+						'irecommendthis_widget_item_count',
+						sprintf(
+							' <span class="irecommendthis-count" aria-label="%1$s">(%2$s)</span>',
+							/* translators: %d: Number of recommendations */
+							esc_attr( sprintf( _n( '%d recommendation', '%d recommendations', $post_count, 'i-recommend-this' ), $post_count ) ),
+							esc_html( $post_count )
+						),
+						$post_count,
+						$item
+					);
 
-			/**
-			 * Action fired after widget item.
-			 *
-			 * @since 4.0.0
-			 * @param object $item The current post item.
-			 */
-			do_action( 'irecommendthis_after_widget_item', $item );
-		}//end foreach
+					echo wp_kses_post( $count_html );
+				}//end if
 
-		echo '</' . esc_attr( $list_tag ) . '>';
+				printf( '</%s>', esc_attr( $item_tag ) );
+
+				/**
+				 * Action fired after widget item.
+				 *
+				 * @since 4.0.0
+				 * @param object $item The current post item.
+				 */
+				do_action( 'irecommendthis_after_widget_item', $item );
+			}//end foreach
+
+			printf( '</%s>', esc_attr( $list_tag ) );
+		}//end if
 
 		/**
 		 * Action fired after widget list.
